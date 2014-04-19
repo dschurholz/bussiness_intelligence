a592b16c77166547d50e937519f1783d9e44b2e7
dschurholz <daniel.schurholz@gmail.com>
2014-04-19 00:39:21 -0500

    Adding time model and some settings needed.

---
+++ bussiness_intelligence/local_settings.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'topicosbd',
+        'USER': 'root',
+        'PASSWORD': 'gaizka',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
--- bussiness_intelligence/settings.py
+++ bussiness_intelligence/settings.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from os.path import dirname, join, realpath
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ROOT_DIR = os.path.realpath(os.path.join(BASE_DIR, '..'))
 
+STATIC_ROOT = ROOT_DIR + '/static/'
+
+MEDIA_ROOT = ROOT_DIR + '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +41,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'core',
+
+    # Dependencies
+    'rest_framework',
+    'south',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,17 +64,6 @@ ROOT_URLCONF = 'bussiness_intelligence.urls'
 
 WSGI_APPLICATION = 'bussiness_intelligence.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -75,8 +77,39 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 20,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.OAuth2Authentication'
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.YAMLRenderer',
+        'rest_framework.renderers.XMLRenderer',
+        'rest_framework_msgpack.renderers.MessagePackRenderer'
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.YAMLParser',
+        'rest_framework.parsers.XMLParser',
+        'rest_framework_msgpack.parsers.MessagePackParser'
+    )
+
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError, exp:
+    from django.core.exceptions import ImproperlyConfigured
+    raise ImproperlyConfigured("local_settings.py is not defined. "
+                               "Did you forget to symlink one?")
