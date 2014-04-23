8e600fd7dbd731078e5c1207ee0168f1840aa990
dschurholz <daniel.schurholz@gmail.com>
2014-04-23 12:02:27 -0500

    Adding more models and admins.

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,49 @@
 from django.contrib import admin
 
-from .models import Time
+from .models import (Customer, DimReference, DimCustomerUnit,
+                     Event, Region, SpeedInfringement, Time)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ['id_customer', 'ds_customer']
+    list_display = ('id_customer', 'company', 'unit',)
+
+
+class DimCustomerUnitAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'unit_plate']
+    list_display = ('unit_plate', 'customer', 'id')
+
+
+class DimReferenceAdmin(admin.ModelAdmin):
+    search_fields = ['id_reference', 'region']
+    list_display = ('region', 'province', 'district',
+                    'road_name', 'id_reference')
+
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ['time', 'total']
+    list_display = ('time', 'region', 'customer',)
+
+
+class RegionAdmin(admin.ModelAdmin):
+    search_fields = ['id_region', 'via']
+    list_display = ('id_region', 'city', 'province', 'district',)
+
+
+class SpeedInfringementAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'unit']
+    list_display = ('unit', 'date', 'speed', 'id')
 
 
 class TimeAdmin(admin.ModelAdmin):
     search_fields = ['id_date', 'ds_time']
     list_display = ('id_date', 'year', 'month', 'day',)
 
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(DimCustomerUnit, DimCustomerUnitAdmin)
+admin.site.register(DimReference, DimReferenceAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(SpeedInfringement, SpeedInfringementAdmin)
 admin.site.register(Time, TimeAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,84 @@
 from django.db import models
 
 
+class Customer(models.Model):
+    id_customer = models.IntegerField(primary_key=True, db_column='id_cliente')
+    company = models.CharField(max_length=50, db_column='empresa', null=True)
+    unit = models.CharField(max_length=50, db_column='unidad', null=True)
+    ds_customer = models.CharField(max_length=10, db_column='ds_cliente',
+                                   null=True)
+
+    class Meta:
+        db_table = 'cliente'
+        ordering = ['id_customer']
+
+    def __unicode__(self):
+        return "%s-%s" % (self.company, self.unit)
+
+
+class DimReference(models.Model):
+    id_reference = models.IntegerField(primary_key=True,
+                                       db_column='idReferencia')
+    region = models.CharField(max_length=110, db_column='sDpto', null=True)
+    province = models.CharField(max_length=100, db_column='sProvi', null=True)
+    province_capital = models.CharField(max_length=100, db_column='sCapProvi',
+                                        null=True)
+    district = models.CharField(max_length=100, db_column='sDistr', null=True)
+    road_type = models.CharField(max_length=2, db_column='sTipVia', null=True)
+    road_name = models.CharField(max_length=100, db_column='sNomVia',
+                                 null=True)
+    road_block = models.CharField(max_length=10, db_column='sCuadVia',
+                                  null=True)
+    poi_reference = models.CharField(max_length=100, db_column='sPoiRef',
+                                     null=True)
+    poi_road = models.CharField(max_length=100, db_column='sPoiVia', null=True)
+    siege = models.CharField(max_length=100, db_column='sCerco', null=True)
+
+    class Meta:
+        db_table = 'dimreferenciasdw'
+        ordering = ['region']
+
+
+class DimCustomerUnit(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='UNI_CODIGO')
+    unit_plate = models.CharField(max_length=9, db_column='UNI_MATRICULA',
+                                  null=True)
+    customer = models.CharField(max_length=160, db_column='CLI_NOMBRE',
+                                null=True)
+
+    class Meta:
+        db_table = 'dimunidadclientedw'
+        ordering = ['customer']
+
+
+class Region(models.Model):
+    id_region = models.IntegerField(primary_key=True)
+    city = models.CharField(max_length=50, db_column='Ciudad', null=True)
+    province = models.CharField(max_length=50, db_column='Provincia',
+                                null=True)
+    district = models.CharField(max_length=50, db_column='Distrito', null=True)
+    road = models.CharField(max_length=50, db_column='Via', null=True)
+
+    class Meta:
+        db_table = 'region'
+        ordering = ['id_region']
+
+    def __unicode__(self):
+        return "%s-%s-%s" % (self.city, self.province, self.district)
+
+
+class SpeedInfringement(models.Model):
+    id = models.IntegerField(primary_key=True)
+    unit = models.ForeignKey(DimCustomerUnit, db_column='UNI_CODIGODW')
+    date = models.DateField(db_column='MEC_FECCOMUNDW', null=True)
+    speed = models.SmallIntegerField(db_column='MEC_VELOCIDAD', null=True)
+    reference = models.ForeignKey(DimReference, db_column='idReferencia')
+    codemensa = models.IntegerField(db_column='MEC_CODMENSA', null=True)
+
+    class Meta:
+        db_table = 'factexcesovelocidad'
+
+
 class Time(models.Model):
     id_date = models.IntegerField(primary_key=True)
     year = models.CharField(max_length=50, db_column='Anho')
@@ -11,3 +89,17 @@ class Time(models.Model):
     class Meta:
         db_table = 'tiempo'
         ordering = ['id_date']
+
+    def __unicode__(self):
+        return "%s-%s-%s" % (self.year, self.month, self.day)
+
+
+class Event(models.Model):
+    time = models.ForeignKey(Time, db_column='id_tiempo', null=True)
+    region = models.ForeignKey(Region, db_column='id_region', null=True)
+    customer = models.ForeignKey(Customer, db_column='id_cliente', null=True)
+    total = models.IntegerField(db_column='cantidad', null=True)
+
+    class Meta:
+        db_table = 'hechos'
+        ordering = ['time', 'region', 'customer', 'total']
