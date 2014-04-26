65b83058dad3b4ae5ee9d1853bbc415f7c5db3f3
Pc <junior@ubuntu.(none)>
2014-04-26 16:46:22 -0500

    Serializers, Views y Urls

--- api/views.py
+++ api/views.py
@@ -6,10 +6,11 @@ from rest_framework import permissions
 from rest_framework import status
 
 from core.models import (Customer, DimCustomerUnit, DimReference,
-                         SpeedInfringement)
+                         SpeedInfringement, Region, Time, Event)
 
 from .serializers import (CustomerSerializer, SpeedInfringementSerializer,
-                          DimCustomerUnitSerializer, DimReferenceSerializer)
+                          DimCustomerUnitSerializer, DimReferenceSerializer,
+                          RegionSerializer, TimeSerializer, EventSerializer)
 
 
 class CustomerList(generics.ListCreateAPIView):
@@ -110,3 +111,78 @@ class SpeedInfringementDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = SpeedInfringement.objects.all()
     serializer_class = SpeedInfringementSerializer
     pk_url_kwarg = 'spinf_id'
+
+
+class RegionList(generics.ListCreateAPIView):
+    """
+    `GET`: Returns a list of all regions.
+
+    `POST`: Add a region.
+    """
+
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+
+
+class RegionDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    `GET`: Returns detail about a region.
+
+    `PUT`: Updates region information.
+
+    `DELETE`: Deletes region.
+    """
+
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+    pk_url_kwarg = 'region_id'
+
+
+class TimeList(generics.ListCreateAPIView):
+    """
+    `GET`: Returns a list of all times.
+
+    `POST`: Add a time.
+    """
+
+    queryset = Time.objects.all()
+    serializer_class = TimeSerializer
+
+
+class TimeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    `GET`: Returns detail about a region.
+
+    `PUT`: Updates region information.
+
+    `DELETE`: Deletes region.
+    """
+
+    queryset = Time.objects.all()
+    serializer_class = TimeSerializer
+    pk_url_kwarg = 'time_id'
+
+
+class EventList(generics.ListCreateAPIView):
+    """
+    `GET`: Returns a list of all events.
+
+    `POST`: Add an event.
+    """
+
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    `GET`: Returns detail about an event.
+
+    `PUT`: Updates event information.
+
+    `DELETE`: Deletes event.
+    """
+
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    pk_url_kwarg = 'event_id'
